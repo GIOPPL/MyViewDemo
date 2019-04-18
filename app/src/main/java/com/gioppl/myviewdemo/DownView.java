ccd02df6d18ad449ec7772c85edf5a94b0ff915c
GIOPPL <1766468447@qq.cim>
2019-04-19 00:39:13 +0800

    第四次更新，添加直线振荡效果。

--- app/src/main/java/com/gioppl/myviewdemo/DownView.java
+++ app/src/main/java/com/gioppl/myviewdemo/DownView.java
@@ -1,7 +1,5 @@
 package com.gioppl.myviewdemo;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
@@ -11,10 +9,8 @@ import android.graphics.Color;
 import android.graphics.CornerPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -36,32 +32,31 @@ public class DownView extends View {
     private Paint mArrowPaint;
     private Paint mTextPaint;
     private Paint circlePaint1, circleLinePaint;
-    private ValueAnimator animator;
     private static int circleAnimatorValue = 1;
+    //动画的value
     private int radius = 0;
-    private RectF mCircleRectF;
+    private int upValue=0;
+    private int upValue2=0;
 
+
+    private enum MoveStatus {
+        STATUS0,
+        STATUS1,
+        STATUS2,
+        STATUS3,
+        STATUS4,
+    }
+    private AnimatorSet animatorSet;
+    private MoveStatus moveStatus = MoveStatus.STATUS0;
 
     //四大控制点,动画value
-    private int controllerValue=0;
-    private long theAnimationExecuteTime=1000;
-    private int controllerValue2=0;
-
-    public DownView(Context context) {
-        super(context);
-        Log.i("DownView", "1");
-        init();
-    }
+    private int controllerValue = 0;
+    private long theAnimationExecuteTime = 500;//动画时间
+    private int controllerValue2 = 0;
 
     public DownView(Context context, AttributeSet attrs) {
         super(context, attrs);
         Log.i("DownView", "2");//这个
-        init();
-    }
-
-    public DownView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        Log.i("DownView", "3");
         init();
     }
 
@@ -104,10 +99,9 @@ public class DownView extends View {
 
         mArrowPath = new Path();
         circleLinePath = new Path();
-        mCircleRectF = new RectF(400, 400, 600, 600);
 
-        pointCircle = new CirclePoint(500, 500, 200);
-        points=new ArrayList<>();
+        pointCircle = new CirclePoint(500, 500, 100);
+        points = new ArrayList<>();
         CirclePoint point0 = new CirclePoint(pointCircle.getX(), pointCircle.getY() - pointCircle.getR());
         CirclePoint point1 = new CirclePoint(pointCircle.getX() - pointCircle.getR(), pointCircle.getY() - pointCircle.getR());
         CirclePoint point2 = new CirclePoint(pointCircle.getX() - pointCircle.getR(), pointCircle.getY());
@@ -126,8 +120,6 @@ public class DownView extends View {
         points.add(point6);
         points.add(point7);
         points.add(point8);
-
-
 
 
 //        CirclePoint point0 = new CirclePoint(pointCircle.getX(), pointCircle.getY() - pointCircle.getR());
@@ -155,8 +147,7 @@ public class DownView extends View {
 //        points.add(point10);
 //        points.add(point11);
 
-
-
+        animatorSet = new AnimatorSet();
     }
 
     @SuppressLint("DrawAllocation")
@@ -164,47 +155,93 @@ public class DownView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.save();
-//        drawFlag(canvas);
-//        drawCircle(canvas);
-
-        drawCircleLine(canvas);
-//        drawCircleLine2(canvas);
+        switch (moveStatus) {
+            case STATUS0:
+                drawCircle(canvas);
+                break;
+            case STATUS1:
+                drawCircleLine(canvas);
+                break;
+            case STATUS2:
+                drawWave(canvas);
+                break;
+            case STATUS3:
+                break;
+            case STATUS4:
+                break;
+        }
     }
+
 
     private void drawCircleLine2(Canvas canvas) {
-        circleLinePath.moveTo(points.get(0).getX(),points.get(0).getY());
-        circleLinePath.cubicTo(points.get(1).getX(),points.get(1).getY(),points.get(2).getX(),points.get(2).getY(),points.get(3).getX(),points.get(3).getY());
-        circleLinePath.moveTo(points.get(3).getX(),points.get(3).getY());
-        circleLinePath.cubicTo(points.get(4).getX(),points.get(4).getY(),points.get(5).getX(),points.get(5).getY(),points.get(6).getX(),points.get(6).getY());
-        circleLinePath.moveTo(points.get(6).getX(),points.get(6).getY());
-        circleLinePath.cubicTo(points.get(7).getX(),points.get(7).getY(),points.get(8).getX(),points.get(8).getY(),points.get(9).getX(),points.get(9).getY());
-        circleLinePath.moveTo(points.get(9).getX(),points.get(9).getY());
-        circleLinePath.cubicTo(points.get(10).getX(),points.get(10).getY(),points.get(11).getX(),points.get(11).getY(),points.get(0).getX(),points.get(0).getY());
+        circleLinePath.moveTo(points.get(0).getX(), points.get(0).getY());
+        circleLinePath.cubicTo(points.get(1).getX(), points.get(1).getY(), points.get(2).getX(), points.get(2).getY(), points.get(3).getX(), points.get(3).getY());
+        circleLinePath.moveTo(points.get(3).getX(), points.get(3).getY());
+        circleLinePath.cubicTo(points.get(4).getX(), points.get(4).getY(), points.get(5).getX(), points.get(5).getY(), points.get(6).getX(), points.get(6).getY());
+        circleLinePath.moveTo(points.get(6).getX(), points.get(6).getY());
+        circleLinePath.cubicTo(points.get(7).getX(), points.get(7).getY(), points.get(8).getX(), points.get(8).getY(), points.get(9).getX(), points.get(9).getY());
+        circleLinePath.moveTo(points.get(9).getX(), points.get(9).getY());
+        circleLinePath.cubicTo(points.get(10).getX(), points.get(10).getY(), points.get(11).getX(), points.get(11).getY(), points.get(0).getX(), points.get(0).getY());
         canvas.drawPath(circleLinePath, circleLinePaint);
-        canvas.drawPoint(points.get(0).getX(),points.get(0).getY(),circlePaint1);
-        canvas.drawPoint(points.get(1).getX(),points.get(1).getY(),circlePaint1);
-        canvas.drawPoint(points.get(2).getX(),points.get(2).getY(),circlePaint1);
-        canvas.drawPoint(points.get(3).getX(),points.get(3).getY(),circlePaint1);
-        canvas.drawPoint(points.get(4).getX(),points.get(4).getY(),circlePaint1);
-        canvas.drawPoint(points.get(5).getX(),points.get(5).getY(),circlePaint1);
-        canvas.drawPoint(points.get(6).getX(),points.get(6).getY(),circlePaint1);
-        canvas.drawPoint(points.get(7).getX(),points.get(7).getY(),circlePaint1);
-        canvas.drawPoint(points.get(8).getX(),points.get(8).getY(),circlePaint1);
-        canvas.drawPoint(points.get(9).getX(),points.get(9).getY(),circlePaint1);
-        canvas.drawPoint(points.get(10).getX(),points.get(10).getY(),circlePaint1);
-        canvas.drawPoint(points.get(11).getX(),points.get(11).getY(),circlePaint1);
+        canvas.drawPoint(points.get(0).getX(), points.get(0).getY(), circlePaint1);
+        canvas.drawPoint(points.get(1).getX(), points.get(1).getY(), circlePaint1);
+        canvas.drawPoint(points.get(2).getX(), points.get(2).getY(), circlePaint1);
+        canvas.drawPoint(points.get(3).getX(), points.get(3).getY(), circlePaint1);
+        canvas.drawPoint(points.get(4).getX(), points.get(4).getY(), circlePaint1);
+        canvas.drawPoint(points.get(5).getX(), points.get(5).getY(), circlePaint1);
+        canvas.drawPoint(points.get(6).getX(), points.get(6).getY(), circlePaint1);
+        canvas.drawPoint(points.get(7).getX(), points.get(7).getY(), circlePaint1);
+        canvas.drawPoint(points.get(8).getX(), points.get(8).getY(), circlePaint1);
+        canvas.drawPoint(points.get(9).getX(), points.get(9).getY(), circlePaint1);
+        canvas.drawPoint(points.get(10).getX(), points.get(10).getY(), circlePaint1);
+        canvas.drawPoint(points.get(11).getX(), points.get(11).getY(), circlePaint1);
 
     }
+
     private void drawCircleLine(Canvas canvas) {
-        circleLinePath.moveTo(points.get(0).getX()-controllerValue,points.get(0).getY());
-        circleLinePath.quadTo(points.get(1).getX(),points.get(1).getY(),points.get(2).getX(),points.get(2).getY()-controllerValue/2);
-        circleLinePath.quadTo(points.get(3).getX(),points.get(3).getY()-controllerValue,points.get(4).getX(),points.get(4).getY()-controllerValue);
-        circleLinePath.quadTo(points.get(5).getX(),points.get(5).getY()-controllerValue,points.get(6).getX(),points.get(6).getY()-controllerValue/2);
-        circleLinePath.quadTo(points.get(7).getX(),points.get(7).getY(),points.get(8).getX()+controllerValue,points.get(8).getY());
-        canvas.drawPath(circleLinePath, circleLinePaint);
-        circleLinePath.reset();
+        if ((controllerValue<pointCircle.getR()*2) && (moveStatus==MoveStatus.STATUS1)){
+            circleLinePath.moveTo(points.get(0).getX() - controllerValue, points.get(0).getY());
+            circleLinePath.quadTo(points.get(1).getX(), points.get(1).getY(), points.get(2).getX(), points.get(2).getY() - controllerValue / 2);
+            circleLinePath.quadTo(points.get(3).getX(), points.get(3).getY() - controllerValue, points.get(4).getX(), points.get(4).getY() - controllerValue);
+            circleLinePath.quadTo(points.get(5).getX(), points.get(5).getY() - controllerValue, points.get(6).getX(), points.get(6).getY() - controllerValue / 2);
+            circleLinePath.quadTo(points.get(7).getX(), points.get(7).getY(), points.get(8).getX() + controllerValue, points.get(8).getY());
+            canvas.drawPath(circleLinePath, circleLinePaint);
+            circleLinePath.reset();
+        }else {
+            moveStatus=MoveStatus.STATUS2;
+            ValueAnimator valueAnimator1 = upArcAnimation();
+            valueAnimator1.start();
+            ValueAnimator valueAnimator2 = upArcAnimation2();
+            valueAnimator2.start();
+            invalidate();
+        }
     }
 
+    int offset=0;
+    //直线振荡
+    private void drawWave(Canvas canvas) {
+        float r = pointCircle.getR();
+        float x = pointCircle.getX();
+        float y = pointCircle.getY()-r;
+        Path wavePath = new Path();
+
+
+        if (moveStatus==MoveStatus.STATUS2) {
+
+                CirclePoint point1=new CirclePoint(x-2*r,y);
+                CirclePoint point2=new CirclePoint(x,y-upValue);
+                CirclePoint point3=new CirclePoint(x+2*r,y);
+                wavePath.moveTo(point1.getX(),point1.getY());
+                wavePath.quadTo(point2.getX(),point2.getY(),point3.getX(),point3.getY());
+                canvas.drawPath(wavePath,circleLinePaint);
+                invalidate();
+
+        }else {
+
+        }
+        offset++;
+    }
+    //画一个箭头
     private void drawFlag(Canvas canvas) {
         int width = getWidth() - 200;
         int height = getHeight() - 200;
@@ -220,63 +257,62 @@ public class DownView extends View {
         canvas.drawText("100%", width / 2 + 35, height / 2 + 70, mTextPaint);
         canvas.drawPath(mArrowPath, mArrowPaint);
     }
-
+    //画圆
     private void drawCircle(Canvas canvas) {
-        int layoutCont = canvas.saveLayer(mCircleRectF, circlePaint1, Canvas.ALL_SAVE_FLAG);
-        canvas.drawCircle(500, 500, 100, circlePaint1);
-        circlePaint1.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
-        canvas.drawCircle(500, 500, radius, circlePaint1);
-        circlePaint1.setXfermode(null);
-        canvas.restoreToCount(layoutCont);
-    }
-    //让圆形上部分展开
-    private void addCircleToLineAnimation_1() {
-        AnimatorSet lineAnimatorSet=new AnimatorSet();
+        if (radius<pointCircle.getR()&& moveStatus==MoveStatus.STATUS0){
+            float x = pointCircle.getX();
+            float y = pointCircle.getY();
+            float r = pointCircle.getR();
+            canvas.drawCircle(x, y, r, circlePaint1);
+            circlePaint1.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+            canvas.drawCircle(x, y, radius, circlePaint1);
+            circlePaint1.setXfermode(null);
+            invalidate();
+        }else {
+            moveStatus=MoveStatus.STATUS1;
+            invalidate();
+        }
 
-        ValueAnimator controllerPointAnimation1=ValueAnimator.ofInt(0, (int) (pointCircle.getR()*2));
-        controllerPointAnimation1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+    }
+    //弧线向上的动画
+    private ValueAnimator upArcAnimation() {
+
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(120, upValue-120);
+        valueAnimator.setDuration(theAnimationExecuteTime);
+        valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        valueAnimator.setRepeatCount(20);//设置重复次数。
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                if (controllerValue>=pointCircle.getR()*2-1){
-                    circleLinePath.close();
-                    circleLinePath.reset();
-                }
-                controllerValue= (int) animation.getAnimatedValue();
+                upValue = (int) animation.getAnimatedValue();
+                Log.i("1111111",upValue+"");
                 invalidate();
             }
         });
-        controllerPointAnimation1.setDuration(theAnimationExecuteTime);
-        controllerPointAnimation1.start();
-//        ValueAnimator controllerPointAnimation2=ValueAnimator.ofInt(0, (int) (pointCircle.getR()*2+10));
-//        controllerPointAnimation1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation) {
-//                controllerValue2= (int) animation.getAnimatedValue();
-//                invalidate();
-//            }
-//        });
-//        controllerPointAnimation1.setDuration(theAnimationExecuteTime);
+        return valueAnimator;
     }
-    //让圆形上部分展开后向上突起
-    private void addCircleToLineAnimation_2() {
-        ValueAnimator controllerPointAnimation1=ValueAnimator.ofInt(0, (int) (pointCircle.getR()*2));
-        controllerPointAnimation1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+    //弧线向上的动画
+    private ValueAnimator upArcAnimation2() {
+
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 120);
+        valueAnimator.setDuration(theAnimationExecuteTime);
+        valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        valueAnimator.setRepeatCount(20);//设置重复次数。
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                controllerValue= (int) animation.getAnimatedValue();
+                upValue2 = (int) animation.getAnimatedValue();
+                Log.i("2222222",upValue2+"");
                 invalidate();
             }
         });
-        controllerPointAnimation1.setDuration(theAnimationExecuteTime);
-        controllerPointAnimation1.start();
+        return valueAnimator;
     }
+    //圆渐变的动画
+    private ValueAnimator addCircleAnimation() {
 
-
-
-    private void addCircleAnimation() {
-        AnimatorSet animatorSet = new AnimatorSet();
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(radius, 90);
-        valueAnimator.setDuration(10000);
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(radius, (int) (pointCircle.getR()));
+        valueAnimator.setDuration(theAnimationExecuteTime);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
@@ -284,63 +320,49 @@ public class DownView extends View {
                 invalidate();
             }
         });
-        animatorSet.playSequentially(valueAnimator);
-        animatorSet.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                super.onAnimationCancel(animation);
-                Log.i("动画", "onAnimationCancel");
-            }
+        return valueAnimator;
+    }
 
+    //让圆形上部分展开动画
+    private ValueAnimator addCircleToLineAnimation_1() {
+        ValueAnimator controllerPointAnimation1 = ValueAnimator.ofInt(0, (int) (pointCircle.getR() * 2));
+        controllerPointAnimation1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                Log.i("动画", "onAnimationEnd");
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-                super.onAnimationRepeat(animation);
-                Log.i("动画", "onAnimationRepeat");
-            }
-
-            @Override
-            public void onAnimationStart(Animator animation) {
-                super.onAnimationStart(animation);
-                Log.i("动画", "onAnimationStart");
-            }
-
-            @Override
-            public void onAnimationPause(Animator animation) {
-                super.onAnimationPause(animation);
-                Log.i("动画", "onAnimationPause");
-            }
-
-            @Override
-            public void onAnimationResume(Animator animation) {
-                super.onAnimationResume(animation);
-                Log.i("动画", "onAnimationResume");
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (controllerValue >= pointCircle.getR() * 2 - 1) {
+                    circleLinePath.close();
+                    circleLinePath.reset();
+                }
+                controllerValue = (int) animation.getAnimatedValue();
+                invalidate();
             }
         });
-        animatorSet.start();
-
-
+        controllerPointAnimation1.setDuration(theAnimationExecuteTime/2);
+        return controllerPointAnimation1;
+//        controllerPointAnimation1.start();
     }
 
-
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-
+    //让圆形上部分展开后向上突起
+    private void addCircleToLineAnimation_2() {
+        ValueAnimator controllerPointAnimation1 = ValueAnimator.ofInt(0, (int) (pointCircle.getR() * 2));
+        controllerPointAnimation1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                controllerValue = (int) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        controllerPointAnimation1.setDuration(theAnimationExecuteTime);
+        controllerPointAnimation1.start();
     }
-
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-//                addCircleAnimation();
-                addCircleToLineAnimation_1();
+                ValueAnimator valueAnimator = addCircleAnimation();
+                ValueAnimator valueAnimator1 = addCircleToLineAnimation_1();
+                animatorSet.play(valueAnimator).before(valueAnimator1);
+                animatorSet.start();
                 break;
             case MotionEvent.ACTION_MOVE:
                 break;
@@ -349,7 +371,4 @@ public class DownView extends View {
         }
         return true;
     }
-
-
-
 }
